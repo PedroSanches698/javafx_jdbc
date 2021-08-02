6e2148e24f01c8395962cde479fdea76ca1ac2fc
PedroSanches698 <PedroSanches698@outlook.com>
2021-08-02 20:39:12 -0300

    Projeto criado

---
+++ src/gui/MainViewController.java
@@ -0,0 +1,40 @@
+package gui;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainViewController implements Initializable {
+
+    @FXML
+    private MenuItem menuItemSeller;
+
+    @FXML
+    private MenuItem menuItemDepartament;
+
+    @FXML
+    private MenuItem menuItemAbout;
+
+    @FXML
+    public void onMenuItemSellerAction(){
+        System.out.println("onMenuItemSellerAction");
+    }
+
+    @FXML
+    public void onMenuItemDepartamentAction(){
+        System.out.println("onMenuItemDepartamentAction");
+    }
+
+    @FXML
+    public void onMenuItemAboutAction(){
+        System.out.println("onMenuItemAboutAction");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
---
+++ src/main/Main.java
@@ -0,0 +1,38 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage){
+
+        try {
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+            ScrollPane scrollPane = loader.load();
+            Scene mainScene = new Scene(scrollPane);
+            primaryStage.setScene(mainScene);
+            primaryStage.setTitle("Sample JavaFx application");
+            primaryStage.getIcons().add(new Image("/image/4732442.jpg"));
+            primaryStage.show();
+
+        }catch(IOException e){
+            e.printStackTrace();
+
+        }
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
